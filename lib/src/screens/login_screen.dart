15b60c18c230bf5ee4926cd8e7759141f08979d0
AsemSaafan <asemsafan@yahoo.com>
2018-09-05 02:40:26 +0200

    - Adding formKey with as GlobalKey<FormState> Instance. - Adding Reset function to the submit button to test it.

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,49 +1,64 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget{
+class LoginScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return LoginScreenState();
   }
-
 }
 
-class LoginScreenState extends State<LoginScreen>{
+class LoginScreenState extends State<LoginScreen> {
+
+  //Reference key help us to reference one specific widget
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return new Container(
       margin: EdgeInsets.all(20.0),
       child: new Form(
-          child: new Column(
-            children: <Widget>[
-              emailField(),
-              passwordField(),
-//              submitButton(),
-            ],
-          ),
+      // apply the form key to the key here
+        key: formKey,
+        child: new Column(
+          children: <Widget>[
+            //Make widgets separately in the bottom and call them here
+            emailField(),
+            passwordField(),
+            Container(margin: EdgeInsets.only(bottom: 25.0),),
+            submitButton(),
+          ],
+        ),
       ),
     );
   }
 
-  Widget emailField(){
+  Widget emailField() {
     return TextFormField(
       keyboardType: TextInputType.emailAddress,
-      decoration:  new InputDecoration(
+      decoration: new InputDecoration(
         labelText: 'Email Address',
         hintText: 'You@example.com',
       ),
     );
   }
-  Widget passwordField(){
-    return TextFormField(
+
+  Widget passwordField() {
+    return new TextFormField(
       obscureText: true,
-      decoration:  new InputDecoration(
+      decoration: new InputDecoration(
         labelText: 'Password',
         hintText: 'Enter Password',
       ),
     );
   }
-//  Widget submitButton(){
-//
-//  }
+
+  Widget submitButton() {
+    return new RaisedButton(
+      color: Colors.greenAccent,
+      onPressed: () {
+        formKey.currentState.reset();
+      },
+      child: new Text('Submit!'),
+    );
+  }
 }
